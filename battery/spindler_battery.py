cf56c204c7629d8bb7cc3c108a59d20982d68d47
Nithes <nithes2003@gmail.com>
2023-10-19 07:55:39 +0530

    Battery module done

---
+++ battery/spindler_battery.py
@@ -0,0 +1,14 @@
+from battery.battery import battery
+from utils import add_years
+
+class SplinderBattery(battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def needs_service(self):
+        date_of_service = add_years(self.last_service_date, 2)
+        if date_of_service < self.current_date:
+            return True
+        else:
+            return False
