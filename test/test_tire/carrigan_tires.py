e70264fbfa6b0b004846910a51e3649b60693f5c
NITHES ARUNKUMAR <nithes2003@gmail.com>
2023-10-20 04:45:00 +0530

    Update carrigan_tires.py

--- test/test_tire/carrigan_tires.py
+++ test/test_tire/carrigan_tires.py
@@ -4,11 +4,11 @@ from tires.carrigan import CarriganTire
 
 class TestCarriganTires(unittest.TestCase):
     def test_needs_service_true(self):
-        array = [0.1,0.3,0.4,0.2]
-        array = CarriganTire(array)
-        self.assertTrue(tire.needs_service())
+        wear_tear = [0.1,0.3,0.4,0.2]
+        array = CarriganTire(wear_tear)
+        self.assertTrue(array.needs_service)
     
     def test_needs_service_false(self):
-        array = [0.1,0.1,0.1,0.2]
-        array = CarriganTire(array)
-        self.assertFalse(tire.needs_service())
+        wear_tear = [0.1,0.1,0.1,0.2]
+        array = CarriganTire(wear_tear)
+        self.assertFalse(array.needs_service)
